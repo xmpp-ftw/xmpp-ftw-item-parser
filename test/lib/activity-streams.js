30d6efc02edbafadb56c259516c22b0d6da1181b
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-01-14 11:11:32 +0000

    Build and parse activitystrea.ms <target/> element

--- test/lib/activity-streams.js
+++ test/lib/activity-streams.js
@@ -11,7 +11,8 @@ parser.setLogger({
     error: function() {}
 })
 
-describe('Parsing posts with \'thread\'', function() {
+/* jshint -W030 */
+describe('Parsing posts with \'activity streams\'', function() {
 
     it('Shouldn\'t act if no thread namespace', function() {
         var entity = { not: 'empty' }
@@ -56,14 +57,14 @@ describe('Parsing posts with \'thread\'', function() {
         entity.should.eql({
             target: {
                 id: 'tag:xmpp-ftw.jit.su,news,item-20130113',
-                objectType: 'comment'
+                'object-type': 'comment'
             }
         })
     })
 
 })
 
-describe('Building stanzas with \'atom\'', function() {
+describe('Building stanzas with \'activity streams\'', function() {
 
     it('Shouldn\'t add elements if no data attribute provided', function() {
         var stanza = ltx.parse('<item><entry xmlns="' + parser.NS_ATOM + '"/></item>')
@@ -88,7 +89,7 @@ describe('Building stanzas with \'atom\'', function() {
             .should.equal(parser.NS_THREAD)
     })
 
-    it('Adds exected <in-reply-to/> element', function() {
+    it('Adds expected <in-reply-to/> element', function() {
         var stanza = ltx.parse('<item><entry xmlns="' + parser.NS_ATOM + '"/></item>')
         var entry = { 'in-reply-to': {
             ref: 'tag:xmpp-ftw,2013:10',
@@ -98,6 +99,28 @@ describe('Building stanzas with \'atom\'', function() {
         parser.build(entry, stanza)
         stanza.root().getChild('entry').attrs['xmlns:thr']
             .should.equal(parser.NS_THREAD)
+        var inReplyTo = stanza.root().getChild('entry').getChild('in-reply-to')
+        inReplyTo.should.exist
+        inReplyTo.attrs.ref.should.equal(entry['in-reply-to'].ref)
+        inReplyTo.attrs.type.should.equal(entry['in-reply-to'].type)
+        inReplyTo.attrs.href.should.equal(entry['in-reply-to'].href)
+    })
+    
+    it('Adds <target/> element and namespace', function() {
+        var stanza = ltx.parse('<item><entry xmlns="' + parser.NS_ATOM + '"/></item>')
+        var entry = {
+            target: {
+                id: 'tag:xmpp-ftw.jit.su,news,item-20130113',
+                'object-type': 'comment'
+            }
+        }
+        parser.build(entry, stanza)
+        stanza.root().getChild('entry').attrs['xmlns:activity']
+            .should.equal(parser.NS_ACTIVITY)
+        var target = stanza.root().getChild('entry').getChild('target')
+        target.should.exist
+        target.getChildText('id').should.equal(entry.target.id)
+        target.getChildText('object-type').should.equal(entry.target['object-type'])
     })
 
 })
